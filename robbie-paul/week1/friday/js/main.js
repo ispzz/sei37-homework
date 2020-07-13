5e38be770482a3daea943ae7f2f6053f1cc0c37c
robertmtpaul <rob@glo.id.au>
2020-07-13 11:21:01 +1000

    Friday MTA homework, further disaster

--- robbie-paul/week1/friday/js/main.js
+++ robbie-paul/week1/friday/js/main.js
@@ -23,90 +23,92 @@
 // Your trip planner must work in either direction, i.e. planTrip('N', 'Times Square', '6', '33rd') (starting at Times Square) should work as well as the reverse trip planTrip('6', '33rd', 'N', 'Times Square') (starting at 33rd).
 //
 
-1.
-2.
-3. 
-  ` `
-
 
 const MTA = {  //stores line names as keys, and station names as values
   
-  'N Line': [
-    'Times Square',
-    '34th',
-    '28th - N Line',
-    '23rd',
-    'Union Square',
-    '8th',
-  ],
+  const network = {
+    N: [
+      'Times Square',
+      '34th',
+      '28th - N Line',
+      '23rd',
+      'Union Square',
+      '8th',
+    ],
 
-  'L Line': [
-    '8th',
-    '6th',
-    'Union Square',
-    '3rd',
-    'Astor Place',
-  ],
+    L: [
+      '8th',
+      '6th',
+      'Union Square',
+      '3rd',
+      'Astor Place',
+    ],
 
-  '6 Line': [
-    'Grand Central',
-    '33rd',
-    '28th - 6 Line',
-    '23rd',
-    'Union Square',
-    'Astor Place',
-  ],
+    6: [
+      'Grand Central',
+      '33rd',
+      '28th - 6 Line',
+      '23rd',
+      'Union Square',
+      'Astor Place',
+    ],
+};
 
-// multi LINE TRIP PROGRAM
-
-// 1. Look for start station in array/ . Eg. Times Square
-// look through line array for whichever input is
-
-// 2. Count stations from 'Times Square' to Union square
-    // get index of times sq; then get index of union square, 
-    // subtract index of times square from union square
-    // Use sliceOf to copy the items into new array and print them to the console.
-
-// 3. look for destination stn in other arrays
-        // keep looking until found destination station
-
-// 4. count stations from union square\ to destination point / 33rd 
-
-// 5. 
+let stepCounter = 0;
 
 
+//  TRIP PROGRAM
+const singleLineTrip = function(lineName, startStation, endStation) {
+  let route = [];
+
+    // 1. Look for start station in array/ . Eg. Times Square &look through line array for whichever input is
+    const currentLine = mta[key]; // Get the line array
+
+    // 2. Count stations from 'Times Square' to Union square; get index of times sq; then get index of union square, 
+    const startIndex = network[line].indexOf( start );  //get the index of the start station.
+    const endIndex = network[line].indexOf( end ); //get the index of the end station.
+        
+    if (startIndex < endIndex ) {    //checks if station is ahead or behind the start station
+
+        }
+    // Copy the stations between startStation and endStation into new array and print them to the console.
+    let route = this.network[line].sliceOf( startStation, endStation);          
+    console.log(`You must travel through the following stops on the ${MTA.key} line: ${route}`);
+    
+    } // singleLineTrip function
 
 
 
+//MULTI LINE TRIP funct
+  
+  const multiLineTrip = function(startLine, startStation, endLine, endStation) {
+    // 1. Look for start station in array
+
+    // 2. look for destination station in all arrays. Keep looking until found destination station. Check if it's on the same line.
+
+    // 2. If station is on another line, count stations from start station to Union square. Print the stations that are between start station and union square. Then, print 'Change at Union Square.'
+
+    // 3. count stations from union square to destination point / 33rd
+
+    // 4.  
+  }
+
+// DRAFT CODE
+
+// let exampleTrip = MTA['N Line'].slice(1, 3);
+// console.log(`You must travel through the following stops on the ${MTA.key} line: ${exampleTrip}`);
 
 
   // const startStation = MTA['6 Line'][2];
   // console.log(startStation);
-
-
-  }
-  const singleLineTrip = function(line, startStation, endStation) {
-    let startStationIndex = line.indexOf(startStation)
-
-    // let station = MTA[key][i].;
+    // let station = MTA[line][i].;
     // console.log(startStation)
-
 //   // const startLine = allLines[startingLineName];
 
-}
+
+// // } 
 
 
-let exampleTrip = MTA['N Line'].slice(1, 3);
-console.log(`You must travel through the following stops on the ${MTA.key} line: ${exampleTrip}`);
-
-
-
-// const multiLineTrip = function() {
-
-// } 
-
-
-// .indexOf(), .slice(), .reverse()
 
 
 
