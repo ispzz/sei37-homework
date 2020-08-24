36e2e49a4212c6331efe59eb771341c7af68cf72
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2020-08-25 09:03:44 +1000

    update

--- chris-stevenson/week8/monday/monday-hw/src/components/MyAir.js
+++ chris-stevenson/week8/monday/monday-hw/src/components/MyAir.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import axios from 'axios';
+import GoogleMapReact from 'google-map-react';
+
+
+
 
 class MyAir extends React.Component{
+
 
   state = {
     result: {
       date: '',
-      coordinates: '',
-    }
+      coordinates: ''
+    },
+    center: {lat: 0, lng: 0},
+    zoom: 11
   }
 
   componentDidMount(){
+    let map;
+    let infoWindow;
     navigator.geolocation.getCurrentPosition((position) => {
-      const lat = position.coords.latitude
-      const long = position.coords.longitude
-      axios.get(`https://api.openaq.org/v1/measurements?coordinates=${lat},${long}&radius=10000`)
+      const myLat = position.coords.latitude;
+      const myLong = position.coords.longitude;
+      this.setState({center: {lat: myLat, lng: myLong}})
+      axios.get(`https://api.openaq.org/v1/measurements?coordinates=${myLat},${myLong}&radius=10000`)
       .then(data => {
         console.log(data.data.results[0]);
-        this.setState({result: data.data.results[0]})
+        this.setState({result: data.data.results[0]});
+        this.setState({zoom: 11});
       })
       .catch(err => console.log(err));
-    });
+
+    }); //navigator
+
   }
 
   render(){
+
+
     return(
       <div>
         <h1>City: {this.state.result.city}</h1>
@@ -33,10 +48,25 @@ class MyAir extends React.Component{
         <h3>Coordinates</h3>
         <p>Latitude: {this.state.result.coordinates.latitude}</p>
         <p>Longitude: {this.state.result.coordinates.longitude}</p>
+        {
+          this.state.center.lat && (
+            <div style={{ height: '30vh', width: '50vh' }}>
+              <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyDMKCpjk0syNql3LikE4VEvny39AM1p3xc' }}
+                defaultCenter={this.state.center}
+                defaultZoom={this.state.zoom}
+                >
+              </GoogleMapReact>
+            </div>
+          )
+        }
 
       </div>
     );
   }//render
 }
+
+
+
 
 export default MyAir
