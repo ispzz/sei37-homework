cdd657e8eaa6f5054066f4fab6e65a5db64479bd
ispzz <zaraii123@gmail.com>
2020-09-15 11:10:39 +1000

    SOLUTION LAST DAY WARMUP :(

--- warmups/week11/day2_markov_text/js/main.js
+++ warmups/week11/day2_markov_text/js/main.js
@@ -1,7 +1,107 @@
-// seperate text into an array of words
-// iterate over the array and add the current word(if the current word already exists as a key don't re-add it) as a key with an empty array as a value to your lookup table
-// get the word that follows this word and push it onto the array of words for this key
+const poem = `
+The dogs are running.
+The dogs are happy.
+The owners are drunk.
+No dogs survived.
+`;
 
-// Pick a random starting word, and get the next word by randomly picking one of the words in its array of following words - Use Math.random to get random words from the array e.g. maybe like text soup hmmmmm
+const sentenceStarterWords = []; // array to store our sentence starters
 
-// Have your page load the contents of a text file into a single variable using AJAX (you will need to run a local server so you can open the file as a URL). To run a local server which will let you load a local text file via AJAX, try `python -m SimpleHTTPServer` on the command line.
+// Before we can create the table - we're first going to need to get our text and split it into an array
+// To create our markov table, we're going to create a js object called table
+// we want an object so we can store each word and it's list of possible words that could be next
+// so to actually add to the table we are going to loop through each word in the array and add the currentWord as a key unless it's already been added
+// The: []
+// then we want to see whatever the following word is and push that into an array as a value
+// The: ["dogs"]
+// and when the loop gets to the next 'The' instead of creating a new key it will just push the word following the current word onto the array that was just created
+const createMarkovTable = text => {
+
+    const table = {};
+
+    let words = text.split(/[ ;\-\n]+/);
+    // console.log(words);
+
+    words = words.filter(word => word.length > 0);
+    console.log(words);
+
+    // const outputArray = [];
+    // for (let i = 0; i < words.length; i++) {
+    //     const word = words[i];
+    //     if (word.length > 0) {
+    //         outputArray.push(word);
+    //     }
+    // }
+    // console.log( outputArray );
+
+    for (let i = 0; i < words.length - 1; i++) {
+        const currentWord = words[i];
+
+        // Check if current word is in the table
+        if (!(currentWord in table)) {
+            // The first time we see this word, set its value to []
+            table[currentWord] = [];
+        }
+
+        // get the word that follows this word
+        const nextWord = words[ i + 1 ];
+        table[currentWord].push( nextWord );
+
+        if( currentWord.match(/^[A-Z][a-z]/) ) {
+            sentenceStarterWords.push( currentWord );
+        }
+        
+    } // end of for loop
+    // console.log(sentenceStarterWords);
+    // console.log(table);
+    return table;
+
+} // end of createMarkovTable
+// createMarkovTable( poem );
+
+// Each array can have multiple instances of each word
+// We can use a random function since duplicated words will have a higher probability of being picked
+const getRandomArrayElement = array => {
+    const randomIndex = Math.floor( Math.random() * array.length );
+    return array[ randomIndex ];
+}
+
+// This function will choose a random word from the array of sentence starters
+// then it will choose one of the following words at random to generate the text
+const generateMarkovText = (table, outputLength) => {
+
+    let currentWord = getRandomArrayElement( sentenceStarterWords );
+    let output = currentWord; // The first word we've chosen above is what starts the output
+
+    for (let i = 0; i < outputLength; i++) {
+        // Pick a new following word for the output sentence by using the current
+        // word as an key into the table (object) of following words, and choosing
+        // a following word from that array, at random
+        const nextWords = table[currentWord];
+        currentWord = getRandomArrayElement( nextWords );
+
+        // Add the next word to the output string
+        output += ' ' + currentWord;
+    }
+
+    return output;
+}; // end of generateMarkovText
+
+// const markovTable = createMarkovTable( poem );
+// console.log(markovTable);
+// const newText = generateMarkovText( markovTable, 3 );
+// console.log(newText);
+
+$.ajax('/text.txt')
+.done( data => {
+  console.log( data.length );
+
+  const markovTable = createMarkovTable( data );
+//   console.log( markovTable );
+
+  window.markov = markovTable; // save to a global variable for debugging
+
+  const newText = generateMarkovText( markovTable, 40 );
+  console.log(`%c${newText}`, 'font-size: 12pt; font-weight: bold');
+});
+
